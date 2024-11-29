703c7f4c7b8dd7b28bb7dbbb9db45430cc178579
rubensprada03 <rubensprada2003@gmail.com>
2024-11-29 11:32:39 -0300

    first commit

--- src/components/SearchContent/SearchContent.jsx
+++ src/components/SearchContent/SearchContent.jsx
@@ -25,6 +25,24 @@ const SearchContent = () => {
             
           </div>
         </div>
+            <div className="form d-flex justify-content-center align-items-center">
+                <div className="input-wrapper">
+                    <img src="path/to/your-icon1.png" alt="Local" className="icon" />
+                    <input type="text" placeholder="Local de partida?" className="input-first" />
+                </div>
+                    <div className="input-wrapper">
+                    <img src="path/to/your-icon2.png" alt="Chegada" className="icon" />
+                <input type="text" placeholder="Chegada" />
+                </div>
+                <div className="input-wrapper">
+                    <img src="path/to/your-icon3.png" alt="Data" className="icon" />
+                    <input type="text" placeholder="Data de ida e volta" />
+                </div>
+                <div className="input-wrapper">
+                    <img src="path/to/your-icon4.png" alt="Adulto" className="icon" />
+                    <input type="text" placeholder="1 adulto" className="input-last" />
+                </div>
+        </div>
       </Global.Wrap>
     </div>
   );
